26a866d5490afcfa0bc00c6d0c7dbb8e72566964
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-21 00:39:23 +0600

    review done

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-const Review = ({review}) => {
-    const {name, img, location} = review;
+const Review = ({ review }) => {
+    const { name, img, location } = review;
     return (
         <div class="card w-96 bg-base-100 shadow-xl">
             <div class="card-body">
-                <h2 class="card-title">Card title!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas unde beatae in distinctio quod a?</p>
+                <div className='flex items-center mt-3'>
+                    <div class="avatar">
+                        <div class="w-16 rounded-full ring ring-primary ring-offset-base-100">
+                            <img src={img} />
+                        </div>
+                    </div>
+                    <div className='ml-5'>
+                        <h3 className='text-2xl'>{name}</h3>
+                        <h4 className='text-xl'>{location}</h4>
+                    </div>
                 </div>
             </div>
         </div>
