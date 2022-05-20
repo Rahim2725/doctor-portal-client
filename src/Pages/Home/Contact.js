e6381a9143a156c591e5ae383cfbda074d1acc4c
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-21 01:37:57 +0600

    part 1 done

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -10,13 +10,13 @@ const Contact = () => {
                 <h4 className="text-xl text-primary text-bold">Contact Us</h4>
                 <h2 className="text-2xl text-white">Stay Connected With us</h2>
                 <form className='mt-8' >
-                    <input className='mb-3 w-full p-2 border-2 border-cyan-400 rounded-md' type="email" name='email' placeholder='Email Address' required />
+                    <input className='mb-4 w-full p-2 border-2 border-cyan-400 rounded-md' type="email" name='email' placeholder='Email Address' required />
                     <br />
-                    <input className='mb-3 w-full p-2 border-2 border-cyan-400 rounded-md' type="text" name='subject' placeholder='Subject' required />
+                    <input className='mb-4 w-full p-2 border-2 border-cyan-400 rounded-md' type="text" name='subject' placeholder='Subject' required />
                     <br />
-                    <textarea className='mb-3 h-32 border-2 border-cyan-400 rounded-md w-full p-2' name="message" id=""  placeholder='Your Message'></textarea>
+                    <textarea className='mb-4 h-32 border-2 border-cyan-400 rounded-md w-full p-2' name="message" id=""  placeholder='Your Message'></textarea>
                     <br />
-                    <input className='mb-3 btn btn-primary bg-gradient-to-r from-primary to-secondary'  type="submit" value="Submit" />
+                    <input className='mb-4 btn btn-primary bg-gradient-to-r from-primary to-secondary'  type="submit" value="Submit" />
 
                 </form>
             </div>
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -9,9 +9,9 @@ import InfoCard from './InfoCard';
 const Info = () => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-            <InfoCard bgclass=" bg-gradient-to-r from-primary to-secondary" text="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock}></InfoCard>
-            <InfoCard bgclass="bg-accent" text="Nithpur, Porsha, Naogaon" cardTitle="Visit our location" img={marker}></InfoCard>
-            <InfoCard bgclass=" bg-gradient-to-r from-primary to-secondary" text="+880 1827251661" cardTitle="Contact us now" img={phone}></InfoCard>
+            <InfoCard bgClass=" bg-gradient-to-r from-primary to-secondary" text="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock}></InfoCard>
+            <InfoCard bgClass="bg-accent" text="Nithpur, Porsha, Naogaon" cardTitle="Visit our location" img={marker}></InfoCard>
+            <InfoCard bgClass=" bg-gradient-to-r from-primary to-secondary" text="+880 1827251661" cardTitle="Contact us now" img={phone}></InfoCard>
         </div>
     );
 };
